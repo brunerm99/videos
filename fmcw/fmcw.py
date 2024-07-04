52d2657f269ce0da33fe9cb1c4a78d815b21547e
Marshall Bruner <brunerm99@gmail.com>
2024-07-04 13:44:42 -0600

    Update title; adding propagation animation; working on pulsed animations

--- fmcw/fmcw.py
+++ fmcw/fmcw.py
@@ -1,6 +1,8 @@
 # fmcw.py
 
 from manim import *
+import numpy as np
+import math
 
 config.background_color = BLACK
 
@@ -74,7 +76,25 @@ class Title(Scene):
                 Create(w_bezier),
             )
         )
-        self.wait(10)
+
+        self.wait(1)
+
+        cw_box = SurroundingRectangle(VGroup(c, w))
+
+        self.play(
+            LaggedStart(
+                Uncreate(f_bezier),
+                Uncreate(m_bezier),
+                Uncreate(c_bezier),
+                Uncreate(w_bezier),
+                Create(cw_box),
+                lag_ratio=0.2,
+            )
+        )
+
+        self.wait(2)
+
+        self.play(FadeOut(cw_box, f, dash, m, c, w, title))
 
 
 class Waveform(Scene):
@@ -84,31 +104,110 @@ class Waveform(Scene):
 
 class PulsedRadarTransmission(Scene):
     def construct(self):
-        radar = (
+        radar = SVGMobject(
+            "./figures/weather-radar.svg",
+            stroke_color=WHITE,
+            color=WHITE,
+            fill_color=WHITE,
+            opacity=1,
+            stroke_width=0.01,
+        ).scale(2)
+        cloud = SVGMobject(
+            "./figures/clouds.svg",
+            stroke_color=WHITE,
+            color=WHITE,
+            fill_color=WHITE,
+            opacity=1,
+            stroke_width=0.01,
+        ).shift(RIGHT * 1 + UP * 2)
+        tree = (
             SVGMobject(
-                "./figures/weather-radar.svg",
+                "./figures/tree.svg",
                 stroke_color=WHITE,
                 color=WHITE,
                 fill_color=WHITE,
                 opacity=1,
+                stroke_width=3,
             )
-            .shift(DOWN / 2)
-            .scale(2)
-        )
-        radar_label = (
-            Tex("Traditional pulsed radar", font_size=DEFAULT_FONT_SIZE)
-            .next_to(radar, direction=UP)
-            .shift(UP / 2)
-        )
-        label_arrow = Arrow(
-            radar_label.get_bottom(),
-            radar.get_top(),
-            stroke_width=50,
-            max_stroke_width_to_length_ratio=10,
+            .shift(RIGHT * 4 + DOWN * 1)
+            .scale(1.5)
         )
 
-        self.play(Create(radar), Write(radar_label, lag_ratio=0.5))
+        self.play(Create(radar))
         self.play(
-            radar.animate.shift(LEFT * 4 + DOWN / 2), radar_label.animate.shift(UP / 2)
+            LaggedStart(
+                radar.animate.shift(LEFT * 5 + DOWN * 2).scale(0.75),
+                AnimationGroup(
+                    Create(cloud, run_time=2),
+                    Create(tree, run_time=2),
+                ),
+                lag_ratio=0.35,
+            )
         )
+
+        radar_pt = radar.get_corner(UP + RIGHT)
+        cloud_pt = cloud.get_corner(LEFT + DOWN)
+        radar_to_cloud = Line(radar_pt, cloud_pt)
+
+        sine = (
+            FunctionGraph(
+                lambda t: np.sin(5 * t),
+                x_range=[0, math.sqrt(np.sum(np.power(cloud_pt - radar_pt, 2)))],
+            )
+            .shift(radar_pt)
+            .rotate(radar_to_cloud.get_angle(), about_point=radar_pt)
+        )
+
+        tracing_dot = Dot(sine.get_start())
+        tracer = TracedPath(
+            tracing_dot.get_center,
+            dissipating_time=0.5,
+            stroke_opacity=[1, 1],
+            stroke_width=6,
+        )
+
+        self.add(tracer)
+        self.play(MoveAlongPath(tracing_dot, sine), rate_func=linear, run_time=2)
+        sine.reverse_points()
+        self.play(MoveAlongPath(tracing_dot, sine), rate_func=linear, run_time=2)
+
+        self.wait(10)
+
+
+class TransmissionTest(Scene):
+    def construct(self):
+        p1 = np.array([-3, -1, 0])
+        p2 = np.array([4, 2, 0])
+        dot1 = Dot(p1)
+        dot2 = Dot(p2)
+
+        line = Line(p1, p2)
+
+        sine = (
+            FunctionGraph(
+                lambda t: np.sin(5 * t),
+                x_range=[0, math.sqrt(np.sum(np.power(p2 - p1, 2)))],
+            )
+            .shift(p1)
+            .rotate(line.get_angle(), about_point=p1)
+        )
+
+        tracing_dot = Dot(sine.get_start())
+        tracer = TracedPath(
+            tracing_dot.get_center,
+            dissipating_time=0.5,
+            stroke_opacity=[1, 1],
+            stroke_width=6,
+        )
+
+        self.add(
+            dot1,
+            dot2,
+            line,
+            tracing_dot,
+            tracer,
+        )
+        self.play(MoveAlongPath(tracing_dot, sine), rate_func=linear, run_time=2)
+        sine.reverse_points()
+        self.play(MoveAlongPath(tracing_dot, sine), rate_func=linear, run_time=2)
         self.wait(10)
