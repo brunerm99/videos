bf27427115f36591b964830b4c342ddb42ce5135
Marshall Bruner <brunerm99@gmail.com>
2025-08-11 08:16:40 -0600

    Working on setting up the problem

--- 13_pulse_compression/pulse_compression.py
+++ 13_pulse_compression/pulse_compression.py
@@ -4,6 +4,8 @@ import sys
 
 from manim import *
 from MF_Tools import VT
+from numpy.fft import fft, fftshift
+from scipy import signal
 from scipy.interpolate import interp1d
 
 sys.path.insert(0, "..")
@@ -29,6 +31,10 @@ def fh(scene, scale=1):
 
 def fw(scene, scale=1):
     return scene.camera.frame.width * scale
+
+
+def lin2db(x):
+    return 10 * np.log10(x)
 
 
 class Issue(MovingCameraScene):
@@ -109,26 +115,27 @@ class Issue(MovingCameraScene):
         xmax = VT(0)
         xmax_t1 = VT(0)
         xmax_t2 = VT(0)
-        pw = 0.2
+        pw = VT(0.2)
         f = 10
         tx = always_redraw(
             lambda: ax.plot(
                 lambda t: np.sin(2 * PI * f * t),
-                x_range=[max(0, ~xmax - pw), ~xmax, 1 / 200],
+                x_range=[max(0, ~xmax - ~pw), ~xmax, 1 / 200],
                 color=TX_COLOR,
             )
         )
+        f_rx = VT(10)
         rx1 = always_redraw(
             lambda: target1_ax.plot(
-                lambda t: 0.5 * np.sin(2 * PI * f * t),
-                x_range=[max(0, ~xmax_t1 - pw), min(~xmax_t1, 1), 1 / 200],
+                lambda t: 0.5 * np.sin(2 * PI * ~f_rx * t),
+                x_range=[max(0, ~xmax_t1 - ~pw), min(~xmax_t1, 1), 1 / 200],
                 color=TARGET1_COLOR,
             )
         )
         rx2 = always_redraw(
             lambda: target2_ax.plot(
-                lambda t: 0.5 * np.sin(2 * PI * f * t),
-                x_range=[max(0, ~xmax_t2 - pw), min(~xmax_t2, 1), 1 / 200],
+                lambda t: 0.5 * np.sin(2 * PI * ~f_rx * t),
+                x_range=[max(0, ~xmax_t2 - ~pw), min(~xmax_t2, 1), 1 / 200],
                 color=TARGET2_COLOR,
             )
         )
@@ -143,7 +150,7 @@ class Issue(MovingCameraScene):
 
         self.wait(0.5)
 
-        pw_line = Line(ax.c2p(~xmax - pw, 1.2), ax.c2p(~xmax, 1.2))
+        pw_line = Line(ax.c2p(~xmax - ~pw, 1.2), ax.c2p(~xmax, 1.2))
         pw_line_l = Line(pw_line.get_start() + DOWN / 8, pw_line.get_start() + UP / 8)
         pw_line_r = Line(pw_line.get_end() + DOWN / 8, pw_line.get_end() + UP / 8)
 
@@ -176,7 +183,7 @@ class Issue(MovingCameraScene):
             pw_label.animate.shift(UP),
             LaggedStart(
                 xmax @ (ax.p2c(target2.get_left())[0]),
-                xmax_t1 @ (pw / 2),
+                xmax_t1 @ (~pw / 2),
                 lag_ratio=0.4,
             ),
         )
@@ -189,8 +196,8 @@ class Issue(MovingCameraScene):
 
         self.play(
             xmax @ 1.5,
-            xmax_t1 @ (0.5 + pw / 2),
-            xmax_t2 @ (0.5 + pw / 2 - target_dist),
+            xmax_t1 @ (0.5 + ~pw / 2),
+            xmax_t2 @ (0.5 + ~pw / 2 - target_dist),
             run_time=3,
         )
 
@@ -198,7 +205,7 @@ class Issue(MovingCameraScene):
 
         target1_pw_line = Line(
             target1_ax.c2p(~xmax_t1, -1),
-            target1_ax.c2p(~xmax_t1 - pw, -1),
+            target1_ax.c2p(~xmax_t1 - ~pw, -1),
         )
         target1_pw_line_l = Line(
             target1_pw_line.get_start() + UP / 6, target1_pw_line.get_start() + DOWN / 6
@@ -209,7 +216,7 @@ class Issue(MovingCameraScene):
 
         target2_pw_line = Line(
             target2_ax.c2p(~xmax_t2, 1),
-            target2_ax.c2p(~xmax_t2 - pw, 1),
+            target2_ax.c2p(~xmax_t2 - ~pw, 1),
         )
         target2_pw_line_l = Line(
             target2_pw_line.get_start() + UP / 6, target2_pw_line.get_start() + DOWN / 6
@@ -224,7 +231,7 @@ class Issue(MovingCameraScene):
             target2_pw_line.get_center(), DOWN, MED_SMALL_BUFF
         )
 
-        self.next_section(skip_animations=skip_animations(False))
+        self.next_section(skip_animations=skip_animations(True))
 
         self.play(
             LaggedStart(
@@ -247,5 +254,248 @@ class Issue(MovingCameraScene):
                 lag_ratio=0.4,
             )
         )
+
+        self.wait(0.5)
+        self.next_section(skip_animations=skip_animations(True))
+
+        line = Line(
+            target2_ax.c2p(~xmax_t2, 0.8), target1_ax.c2p(~xmax_t1 - ~pw * 1.05, -0.6)
+        )
+
+        overlap = Polygon(
+            line.get_start(),
+            [line.get_start()[0], line.get_end()[1], 0],
+            line.get_end(),
+            [line.get_end()[0], line.get_start()[1], 0],
+            fill_opacity=0.4,
+            fill_color=YELLOW,
+            stroke_opacity=0,
+        )
+
+        self.play(FadeIn(overlap))
+
+        self.wait(0.5)
+
+        pw_scale_factor = 4
+        target1_pw_line_new = Line(
+            target1_ax.c2p(~xmax_t1, -1),
+            target1_ax.c2p(~xmax_t1 - ~pw / pw_scale_factor, -1),
+        )
+        target2_pw_line_new = Line(
+            target2_ax.c2p(~xmax_t2, 1),
+            target2_ax.c2p(~xmax_t2 - ~pw / pw_scale_factor, 1),
+        )
+
+        self.play(
+            LaggedStart(
+                FadeOut(overlap),
+                AnimationGroup(
+                    f_rx @ 30,
+                    pw @ (~pw / pw_scale_factor),
+                    Transform(target1_pw_line, target1_pw_line_new),
+                    Transform(
+                        target1_pw_line_l,
+                        Line(
+                            target1_pw_line_new.get_start() + UP / 6,
+                            target1_pw_line_new.get_start() + DOWN / 6,
+                        ).rotate(target1_pw_line_new.get_angle()),
+                    ),
+                    Transform(
+                        target1_pw_line_r,
+                        Line(
+                            target1_pw_line_new.get_end() + UP / 6,
+                            target1_pw_line_new.get_end() + DOWN / 6,
+                        ).rotate(target1_pw_line_new.get_angle()),
+                    ),
+                    Transform(target2_pw_line, target2_pw_line_new),
+                    Transform(
+                        target2_pw_line_l,
+                        Line(
+                            target2_pw_line_new.get_start() + UP / 6,
+                            target2_pw_line_new.get_start() + DOWN / 6,
+                        ).rotate(target2_pw_line.get_angle()),
+                    ),
+                    Transform(
+                        target2_pw_line_r,
+                        Line(
+                            target2_pw_line_new.get_end() + UP / 6,
+                            target2_pw_line_new.get_end() + DOWN / 6,
+                        ).rotate(target2_pw_line.get_angle()),
+                    ),
+                    pw_label_target1[0].animate.next_to(
+                        target1_pw_line_new, UP, MED_SMALL_BUFF
+                    ),
+                    pw_label_target2[0].animate.next_to(
+                        target2_pw_line_new, DOWN, MED_SMALL_BUFF
+                    ),
+                ),
+                lag_ratio=0.4,
+            )
+        )
+
+        self.wait(0.5)
+        self.next_section(skip_animations=skip_animations(True))
+
+        self.play(
+            LaggedStart(
+                FadeOut(
+                    target1_pw_line,
+                    target2_pw_line,
+                    target1_pw_line_r,
+                    target1_pw_line_l,
+                    target2_pw_line_r,
+                    target2_pw_line_l,
+                    pw_label_target1[0],
+                    pw_label_target2[0],
+                ),
+                AnimationGroup(
+                    xmax_t1 @ (1 + ~pw),
+                    xmax_t2 @ (1 + ~pw),
+                ),
+                lag_ratio=0.3,
+            )
+        )
+
+        self.wait(0.5)
+
+        fs = 100
+        f = 25
+        noise_std = VT(0.1)
+
+        x_len = config.frame_width * 0.4
+        y_len = config.frame_height * 0.4
+        y_max = 30
+
+        ax = Axes(
+            x_range=[0, fs / 2, fs / 8],
+            y_range=[0, y_max, 10],
+            tips=False,
+            axis_config={
+                "include_numbers": False,
+            },
+            x_length=x_len,
+            y_length=y_len,
+        ).next_to(self.camera.frame, UP)
+
+        stop_time = 4
+        N = stop_time * fs
+        t = np.linspace(0, stop_time, N)
+        fft_len = N * 8
+        freq = np.linspace(-fs / 2, fs / 2, fft_len)
+        amp = VT(0.1)
+
+        def fft_updater():
+            np.random.seed(1)
+            noise = np.random.normal(loc=0, scale=~noise_std, size=N)
+            x_n = (~amp * np.sin(2 * PI * f * t) + noise) * signal.windows.blackman(N)
+            X_k = fftshift(fft(x_n, fft_len))
+            X_k /= N / 2
+            X_k = np.abs(X_k)
+            X_k = np.clip(10 * np.log10(X_k) + y_max, 0, None)
+            f_X_k_log = interp1d(freq, X_k, fill_value="extrapolate")
+
+            plot = ax.plot(f_X_k_log, x_range=[0, fs / 2, 1 / 100], color=RX_COLOR)
+            return plot
+
+        X_k_plot = always_redraw(fft_updater)
+
+        self.add(ax, X_k_plot)
+
+        new_ax_copy = ax.copy().move_to(self.camera.frame.get_top()).shift(DOWN * 0.9)
+
+        new_scene = Group(radar.vgroup, target1, target2, new_ax_copy)
+
+        snr_line = always_redraw(
+            lambda: Line(
+                ax.c2p(f + 3, -lin2db(~noise_std)),
+                [ax.c2p(f + 3, 0)[0], ax.input_to_graph_point(f, X_k_plot)[1], 0],
+            )
+        )
+        snr_line_u = always_redraw(
+            lambda: Line(
+                snr_line.get_top() + LEFT / 8,
+                snr_line.get_top() + RIGHT / 8,
+            )
+        )
+        snr_line_d = always_redraw(
+            lambda: Line(
+                snr_line.get_bottom() + LEFT / 8,
+                snr_line.get_bottom() + RIGHT / 8,
+            )
+        )
+        snr_label = always_redraw(
+            lambda: Text("SNR", font=FONT).next_to(snr_line, RIGHT, MED_SMALL_BUFF)
+        )
+        self.add(snr_line, snr_line_u, snr_line_d, snr_label)
+
+        self.next_section(skip_animations=skip_animations(False))
+
+        self.play(
+            LaggedStart(
+                AnimationGroup(
+                    ax.animate.move_to(self.camera.frame.get_top()).shift(DOWN * 0.9),
+                    self.camera.frame.animate.scale_to_fit_height(
+                        new_scene.height * 1.2
+                    )
+                    .move_to(new_scene)
+                    .set_x(0),
+                ),
+            )
+        )
+
+        self.wait(0.5)
+
+        pulse_ax = (
+            Axes(
+                x_range=[0, 1, 0.5],
+                y_range=[-1, 1, 0.5],
+                tips=False,
+                x_length=x_len,
+                y_length=y_len,
+            )
+            .set_z_index(-1)
+            .next_to(self.camera.frame, LEFT)
+            .set_y(ax.get_y())
+        )
+
+        pw_plot = VT(0.1)
+        pulse_amp = VT(0.3)
+        pulse_f = 20
+        pulse = always_redraw(
+            lambda: pulse_ax.plot(
+                lambda t: ~pulse_amp * np.sin(2 * PI * pulse_f * t)
+                if t < ~pw_plot
+                else 0,
+                x_range=[0, 1, 1 / 200],
+                color=TX_COLOR,
+            )
+        )
+        self.add(pulse_ax, pulse)
+
+        self.play(
+            Group(pulse_ax, ax).animate.arrange(RIGHT, MED_LARGE_BUFF).set_y(ax.get_y())
+        )
+
+        tx_pulse_label = Text("Transmit Pulse", font=FONT).next_to(pulse_ax, DOWN)
+
+        self.wait(0.5)
+
+        self.play(Write(tx_pulse_label))
+
+        self.wait(0.5)
+
+        self.play(pulse_amp @ (~pulse_amp * 3), amp @ (~amp * 3))
+
+        self.wait(0.5)
+
+        self.play(pulse_amp @ (~pulse_amp / 3), amp @ (~amp / 3))
+
+        self.wait(0.5)
+
+        self.play(pw_plot @ (~pw_plot * 3), amp @ (~amp * 3))
+
+        self.wait(0.5)
+
+        self.play(pw_plot @ (~pw_plot / 3), amp @ (~amp / 3))
 
         self.wait(2)
