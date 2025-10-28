24c9512340b4c8f7983d20ce8c6e389108efbab9
Marshall Bruner <brunerm99@gmail.com>
2025-10-28 09:44:06 -0600

    Started amp gain video

---
+++ 14_amp_gain/amp_gain.py
@@ -0,0 +1,48 @@
+# amp_gain.py
+
+import os
+import sys
+
+import pandas as pd
+from dotenv import load_dotenv
+from manim import *
+from MF_Tools import VT
+from numpy.fft import fft, fftshift
+from scipy import signal
+from scipy.interpolate import interp1d
+
+sys.path.insert(0, "..")
+from props import WeatherRadarTower, get_blocks
+from props.style import BACKGROUND_COLOR, IF_COLOR, RX_COLOR, TX_COLOR
+
+config.background_color = BACKGROUND_COLOR
+
+SKIP_ANIMATIONS_OVERRIDE = False
+
+load_dotenv("../.env")
+FONT = os.getenv("FONT")
+
+BLOCKS = get_blocks()
+
+GOOD = BLUE
+OK = GREY
+BAD = RED
+TARGET1_COLOR = GREEN
+TARGET2_COLOR = ORANGE
+TARGET3_COLOR = BLUE
+
+
+def skip_animations(b):
+    return b and (not SKIP_ANIMATIONS_OVERRIDE)
+
+
+def fh(scene, scale=1):
+    return scene.camera.frame.height * scale
+
+
+def fw(scene, scale=1):
+    return scene.camera.frame.width * scale
+
+
+def lin2db(x):
+    return 10 * np.log10(x)
