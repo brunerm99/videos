c6433d9d2b9b98ab67a91bc3d0f6923a688a2a52
Marshall Bruner <brunerm99@gmail.com>
2026-03-11 00:43:38 -0600

    dk

--- 16_dual_pol/dual_pol.py
+++ 16_dual_pol/dual_pol.py
@@ -12,7 +12,7 @@ from MF_Tools import VT
 from networkx import center
 from numpy.fft import fft, fftshift
 from scipy import signal
-from scipy.interpolate import interp1d
+from scipy.interpolate import PchipInterpolator, interp1d
 
 sys.path.insert(0, "..")
 from props import (
@@ -53,6 +53,9 @@ INPUT_COLOR = BLUE
 OUTPUT_COLOR = ORANGE
 GAIN_COLOR = GREEN
 PAE_COLOR = YELLOW
+PRECIP_COLOR = BLUE
+H_COLOR = GREEN
+V_COLOR = ORANGE
 
 
 def skip_animations(b):
@@ -228,5 +231,276 @@ class DualPol(ThreeDScene):
             ),
             run_time=10,
         )
+
+        self.wait(2)
+
+
+class DropShape(Scene):
+    def construct(self):
+        Deq_min = 1.5
+        Deq_max = 6
+        Deq_target = 5.25
+        Deq = VT(Deq_min)
+
+        def c1(Deq):
+            return (1 / np.pi) * (0.02914 * Deq**2 + 0.9263 * Deq + 0.07791)
+
+        def c2(Deq):
+            return -0.01938 * Deq**2 + 0.4698 * Deq + 0.09538
+
+        def c3(Deq):
+            return -0.06123 * Deq**3 + 1.3880 * Deq**2 - 10.41 * Deq + 28.34
+
+        def c4(Deq):
+            if Deq > 4:
+                return -0.01352 * Deq**3 + 0.2014 * Deq**2 - 0.8964 * Deq + 1.226
+            elif 1.5 <= Deq <= 4:
+                return 0
+            else:
+                return np.nan
+
+        def x_function(Deq, y):
+            c1_val = c1(Deq)
+            c2_val = c2(Deq)
+            c3_val = c3(Deq)
+            c4_val = c4(Deq)
+
+            if np.abs(y / c2_val) > 1:
+                return np.nan
+
+            term1 = np.sqrt(1 - (y / c2_val) ** 2)
+            term2 = np.arccos(y / (c3_val * c2_val))
+            term3 = c4_val * (y / c2_val) ** 2 + 1
+
+            return c1_val * term1 * term2 * term3
+
+        y_values = np.linspace(-2.5, 2.5, 10_000)
+
+        x_values_positive = [x_function(~Deq, y) for y in y_values]
+        x_values_negative = [-x_function(~Deq, y) for y in y_values]
+
+        xvn = np.array(x_values_negative)
+        ind = np.where(~np.isnan(xvn))
+        xvn = xvn[ind]
+        yvn = np.array(y_values)[ind]
+
+        xvp = np.array(x_values_positive)
+        ind = np.where(~np.isnan(xvp))
+        xvp = xvp[ind]
+        yvp = np.array(y_values)[ind]
+
+        f_vn = interp1d(xvn, yvn, fill_value="extrapolate")
+        f_vp = interp1d(xvp, yvp, fill_value="extrapolate")
+
+        rain_ax = NumberPlane(
+            x_range=[-4, 4, 1],
+            y_range=[-4, 4, 1],
+            x_length=4.5,
+            y_length=4.5,
+            background_line_style={
+                "stroke_color": LIGHT_GRAY,
+                "stroke_opacity": 0.6,
+            },
+        )
+
+        hail_ax = rain_ax.copy()
+
+        rain_label = Tex("Rain Drop").next_to(rain_ax, UP)
+        hail_label = Tex("Hail").next_to(hail_ax, UP)
+
+        rain_group = VGroup(rain_label, rain_ax)
+
+        lplot = rain_ax.plot_line_graph(
+            xvn,
+            yvn,
+            line_color=PRECIP_COLOR,
+            add_vertex_dots=False,
+        )
+        rplot = rain_ax.plot_line_graph(
+            xvp,
+            yvp,
+            line_color=PRECIP_COLOR,
+            add_vertex_dots=False,
+        )
+
+        def get_plot_updater(scalar=1):
+            def updater(m: Mobject):
+                x_values = np.array([scalar * x_function(~Deq, y) for y in y_values])
+                ind = np.where(~np.isnan(x_values))
+                xv = x_values[ind]
+                yv = np.array(y_values)[ind]
+                m.become(
+                    rain_ax.plot_line_graph(
+                        xv, yv, add_vertex_dots=False, line_color=PRECIP_COLOR
+                    )
+                )
+
+            return updater
+
+        lplot.add_updater(get_plot_updater(scalar=-1))
+        rplot.add_updater(get_plot_updater(scalar=1))
+
+        # Deq_nl = NumberLine(
+        #     x_range=[Deq_min, Deq_max, 0.5],
+        #     include_numbers=True,
+        #     include_tip=False,
+        #     length=config["frame_width"] * 0.6,
+        # ).to_edge(DOWN)
+
+        # def clamp_drop_y(y):
+        #     drop_half_height = c2(~Deq)
+        #     return np.clip(y, -0.92 * drop_half_height, 0.92 * drop_half_height)
+
+        # def drop_half_width(y):
+        #     return x_function(~Deq, clamp_drop_y(y))
+
+        # def build_updraft_streamline(
+        #     side,
+        #     start_x_factor,
+        #     lower_gap,
+        #     upper_gap,
+        #     top_gap,
+        #     start_pad,
+        #     end_pad,
+        #     lower_y_factor,
+        #     upper_y_factor,
+        #     stroke_width,
+        #     stroke_opacity,
+        #     tip_scale,
+        # ):
+        #     drop_half_height = c2(~Deq)
+        #     max_half_width = drop_half_width(0)
+        #     lower_y = clamp_drop_y(lower_y_factor * drop_half_height)
+        #     mid_lower_y = clamp_drop_y(0.55 * lower_y_factor * drop_half_height)
+        #     shoulder_y = clamp_drop_y(-0.08 * drop_half_height)
+        #     upper_y = clamp_drop_y(upper_y_factor * drop_half_height)
+        #     mid_upper_y = clamp_drop_y(0.5 * (upper_y + 0.82 * drop_half_height))
+        #     top_y = clamp_drop_y(0.82 * drop_half_height)
+        #     start_x = 0.3 * start_x_factor * max_half_width + 0.7 * (
+        #         drop_half_width(lower_y) + 0.35 * lower_gap
+        #     )
+        #     end_x = drop_half_width(top_y) + 0.75 * top_gap
+
+        #     y_knots = np.array(
+        #         [
+        #             -drop_half_height - start_pad,
+        #             lower_y,
+        #             mid_lower_y,
+        #             shoulder_y,
+        #             upper_y,
+        #             mid_upper_y,
+        #             top_y,
+        #             drop_half_height + end_pad,
+        #         ]
+        #     )
+        #     x_knots = np.array(
+        #         [
+        #             start_x,
+        #             drop_half_width(lower_y) + lower_gap,
+        #             drop_half_width(mid_lower_y) + 0.7 * lower_gap + 0.3 * upper_gap,
+        #             drop_half_width(shoulder_y) + upper_gap,
+        #             drop_half_width(upper_y) + upper_gap,
+        #             drop_half_width(mid_upper_y) + 0.45 * top_gap + 0.55 * upper_gap,
+        #             drop_half_width(top_y) + top_gap,
+        #             end_x,
+        #         ]
+        #     )
+        #     x_interp = PchipInterpolator(y_knots, x_knots)
+
+        #     curve = ParametricFunction(
+        #         lambda y: rain_ax.c2p(side * float(x_interp(y)), y),
+        #         t_range=[y_knots[0], y_knots[-1], (y_knots[-1] - y_knots[0]) / 120],
+        #     ).set_stroke(
+        #         WHITE,
+        #         width=stroke_width,
+        #         opacity=stroke_opacity,
+        #     )
+        #     if tip_scale <= 0:
+        #         return curve
+
+        #     tip_anchor = rain_ax.c2p(side * x_knots[-1], y_knots[-1])
+        #     tail_anchor = rain_ax.c2p(side * x_knots[-2], y_knots[-2])
+        #     tangent = normalize(tip_anchor - tail_anchor)
+        #     tip = Triangle(
+        #         fill_color=WHITE,
+        #         fill_opacity=stroke_opacity,
+        #         stroke_width=0,
+        #     ).scale(tip_scale)
+        #     tip.rotate(angle_of_vector(tangent) - PI / 2)
+        #     tip.shift(tip_anchor - tip.get_boundary_point(tangent))
+        #     return VGroup(curve, tip)
+
+        # streamline_specs = [
+        #     dict(
+        #         start_x_factor=0.24,
+        #         lower_gap=0.34,
+        #         upper_gap=0.34,
+        #         top_gap=0.44,
+        #         start_pad=3,
+        #         end_pad=3,
+        #         lower_y_factor=-0.5,
+        #         upper_y_factor=0.1,
+        #         stroke_width=2.5,
+        #         stroke_opacity=0.58,
+        #         tip_scale=0,
+        #     ),
+        #     dict(
+        #         start_x_factor=0.48,
+        #         lower_gap=0.56,
+        #         upper_gap=0.56,
+        #         top_gap=0.72,
+        #         start_pad=3,
+        #         end_pad=3,
+        #         lower_y_factor=-0.5,
+        #         upper_y_factor=0.1,
+        #         stroke_width=2.2,
+        #         stroke_opacity=0.42,
+        #         tip_scale=0,
+        #     ),
+        #     dict(
+        #         start_x_factor=0.72,
+        #         lower_gap=0.78,
+        #         upper_gap=0.78,
+        #         top_gap=1,
+        #         start_pad=3,
+        #         end_pad=3,
+        #         lower_y_factor=-0.5,
+        #         upper_y_factor=0.1,
+        #         stroke_width=1.8,
+        #         stroke_opacity=0.24,
+        #         tip_scale=0,
+        #     ),
+        # ]
+        # wind_lines = always_redraw(
+        #     lambda: VGroup(
+        #         *(
+        #             build_updraft_streamline(side=side, **spec)
+        #             for spec in streamline_specs
+        #             for side in (-1, 1)
+        #         ),
+        #     ).set_z_index(-1)
+        # )
+
+        self.add(
+            # wind_lines,
+            lplot,
+            rplot,
+        )
+
+        # # self.play(
+        # #     Create(rain_ax),
+        # #     Create(lplot),
+        # #     Create(rplot),
+        # # )
+
+        # b1 = always_redraw(
+        #     lambda: CubicBezier(
+        #         rain_ax.i2gp(0.1, rplot),
+        #         rain_ax.i2gp(0.1, rplot),
+        #     )
+        # )
+        self.add(Dot(rain_ax.c2p(0.1, f_vp(0.1))))
+
+        self.play(Deq @ Deq_target, run_time=6)
 
         self.wait(2)
