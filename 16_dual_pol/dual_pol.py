c105d7d4ef98d591d3d7e963e48029abf6ec51a6
Marshall Bruner <brunerm99@gmail.com>
2026-03-30 23:16:01 -0600

    drop shape

--- 16_dual_pol/dual_pol.py
+++ 16_dual_pol/dual_pol.py
@@ -2944,123 +2944,84 @@ def _thurai_drop_outline_points(deq, n_samples=721):
     return right_side + left_side
 
 
-class DropShape(Scene):
-    def construct(self):
-        Deq_min = 1.5
-        Deq_max = 6
-        Deq_target = 6
-        Deq = VT(Deq_min)
+def _thurai_drop_flow_lines(deq):
+    c2_val = _thurai_drop_c2(deq)
+    y_values = np.linspace(-c2_val, c2_val, 361)
+    max_half_width = float(np.nanmax(_thurai_drop_half_width(deq, y_values)))
+    flow_color = interpolate_color(PRECIP_COLOR, WHITE, 0.22)
+    line_specs = [
+        dict(offset=0.18 * max_half_width, top_y=1.22, bot_y=-1.18, opacity=0.65),
+        dict(offset=0.34 * max_half_width, top_y=1.34, bot_y=-1.32, opacity=0.42),
+    ]
+    side_y_levels = np.array([0.86, 0.46, -0.08, -0.52, -0.84]) * c2_val
 
-        def c1(Deq):
-            return (1 / np.pi) * (0.02914 * Deq**2 + 0.9263 * Deq + 0.07791)
+    lines = VGroup()
+    for side in (-1, 1):
+        for idx, spec in enumerate(line_specs):
+            x_levels = _thurai_drop_half_width(deq, side_y_levels)
+            flow_line = VMobject()
+            flow_line.set_points_smoothly(
+                [
+                    np.array(
+                        [
+                            side * (x_levels[0] + spec["offset"] * 0.05),
+                            spec["top_y"] * c2_val,
+                            0.0,
+                        ]
+                    ),
+                    np.array(
+                        [
+                            side * (x_levels[0] + spec["offset"] * 0.22),
+                            side_y_levels[0] + 0.04 * c2_val,
+                            0.0,
+                        ]
+                    ),
+                    np.array(
+                        [
+                            side * (x_levels[1] + spec["offset"]),
+                            side_y_levels[1],
+                            0.0,
+                        ]
+                    ),
+                    np.array(
+                        [
+                            side * (x_levels[2] + spec["offset"] * 0.95),
+                            side_y_levels[2],
+                            0.0,
+                        ]
+                    ),
+                    np.array(
+                        [
+                            side * (x_levels[3] + spec["offset"] * 0.55),
+                            side_y_levels[3],
+                            0.0,
+                        ]
+                    ),
+                    np.array(
+                        [
+                            side * (x_levels[4] + spec["offset"] * (0.18 + 0.05 * idx)),
+                            spec["bot_y"] * c2_val,
+                            0.0,
+                        ]
+                    ),
+                ]
+            )
+            flow_line.set_stroke(
+                flow_color,
+                width=DEFAULT_STROKE_WIDTH * (2.1 - 0.2 * idx),
+                opacity=spec["opacity"],
+            )
+            lines.add(flow_line)
 
-        def c2(Deq):
-            return -0.01938 * Deq**2 + 0.4698 * Deq + 0.09538
-
-        def c3(Deq):
-            return -0.06123 * Deq**3 + 1.3880 * Deq**2 - 10.41 * Deq + 28.34
-
-        def c4(Deq):
-            if Deq > 4:
-                return -0.01352 * Deq**3 + 0.2014 * Deq**2 - 0.8964 * Deq + 1.226
-            elif 1.5 <= Deq <= 4:
-                return 0
-            else:
-                return np.nan
-
-        def x_function(Deq, y):
-            c1_val = c1(Deq)
-            c2_val = c2(Deq)
-            c3_val = c3(Deq)
-            c4_val = c4(Deq)
-
-            if np.abs(y / c2_val) > 1:
-                return np.nan
-
-            term1 = np.sqrt(1 - (y / c2_val) ** 2)
-            term2 = np.arccos(y / (c3_val * c2_val))
-            term3 = c4_val * (y / c2_val) ** 2 + 1
-
-            return c1_val * term1 * term2 * term3
-
-        y_values = np.linspace(-2.5, 2.5, 10_000)
-
-        x_values_positive = [x_function(~Deq, y) for y in y_values]
-        x_values_negative = [-x_function(~Deq, y) for y in y_values]
-
-        xvn = np.array(x_values_negative)
-        ind = np.where(~np.isnan(xvn))
-        xvn = xvn[ind]
-        yvn = np.array(y_values)[ind]
-
-        xvp = np.array(x_values_positive)
-        ind = np.where(~np.isnan(xvp))
-        xvp = xvp[ind]
-        yvp = np.array(y_values)[ind]
-
-        f_vn = interp1d(xvn, yvn, fill_value="extrapolate")
-        f_vp = interp1d(xvp, yvp, fill_value="extrapolate")
-
-        rain_ax = NumberPlane(
-            x_range=[-4, 4, 1],
-            y_range=[-4, 4, 1],
-            x_length=4.5,
-            y_length=4.5,
-            background_line_style={
-                "stroke_color": LIGHT_GRAY,
-                "stroke_opacity": 0.6,
-            },
-        )
-
-        hail_ax = rain_ax.copy()
-
-        rain_label = Tex("Rain Drop").next_to(rain_ax, UP)
-        hail_label = Tex("Hail").next_to(hail_ax, UP)
-
-        rain_group = VGroup(rain_label, rain_ax)
-
-        lplot = rain_ax.plot_line_graph(
-            xvn,
-            yvn,
-            line_color=PRECIP_COLOR,
-            add_vertex_dots=False,
-        )
-        rplot = rain_ax.plot_line_graph(
-            xvp,
-            yvp,
-            line_color=PRECIP_COLOR,
-            add_vertex_dots=False,
-        )
-
-        def get_plot_updater(scalar=1):
-            def updater(m: Mobject):
-                x_values = np.array([scalar * x_function(~Deq, y) for y in y_values])
-                ind = np.where(~np.isnan(x_values))
-                xv = x_values[ind]
-                yv = np.array(y_values)[ind]
-                m.become(
-                    rain_ax.plot_line_graph(
-                        xv, yv, add_vertex_dots=False, line_color=PRECIP_COLOR
-                    )
-                )
-
-            return updater
-
-        lplot.add_updater(get_plot_updater(scalar=-1))
-        rplot.add_updater(get_plot_updater(scalar=1))
-
-        self.add(lplot, rplot)
-
-        # self.play(Deq @ Deq_target, run_time=6)
-
-        self.wait(2)
+    return lines
 
 
-class DropShapeV2(Scene):
+class DropShape(MovingCameraScene):
     def construct(self):
         deq_min = 1.5
         deq_target = 6.0
         deq = VT(deq_min)
+        drop_scale = 0.95
 
         def make_drop_shape():
             points = _thurai_drop_outline_points(~deq)
@@ -3071,16 +3032,24 @@ class DropShapeV2(Scene):
                 fill_opacity=0.12,
                 stroke_width=DEFAULT_STROKE_WIDTH * 2.5,
             )
-            drop.scale(0.95)
+            drop.scale(drop_scale)
+            drop.set_z_index(2)
             return drop
 
+        def make_flow_lines():
+            flow_lines = _thurai_drop_flow_lines(~deq)
+            flow_lines.scale(drop_scale)
+            flow_lines.set_z_index(1)
+            return flow_lines
+
+        flow_lines = always_redraw(make_flow_lines)
         drop_shape = always_redraw(make_drop_shape)
 
-        self.add(drop_shape)
+        self.add(flow_lines, drop_shape)
 
-        # self.play(deq @ deq_target, run_time=4)
+        self.play(deq @ deq_target, run_time=4)
 
-        self.wait(1)
+        self.wait(0.5)
 
 
 def _get_nexrad_archive_volume(
